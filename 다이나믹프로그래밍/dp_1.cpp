5b9243fcfc2f3687695fd95ba08e45e4a6e900d2
uniye <92070609+uniye@users.noreply.github.com>
2023-06-04 14:07:38 +0900

    Add files via upload

---
+++ 다이나믹프로그래밍/dp_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//피보나치
+//시간복잡도 - O(2^n)
+ 
+int dp(int x) {
+	if (x == 1) return 1;
+	if (x == 2) return 1;
+	return dp(x - 1) + dp(x - 2);
+}
+
+int main() {
+	cout << dp(20);
+	return 0;
+}
+
---
+++ 다이나믹프로그래밍/dp_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//피보나치
+//시간복잡도 - O(n)
+long long d[100];
+
+long long dp(int x) {
+	if (x == 1) return 1;
+	if (x == 2) return 1;
+	if (d[x] != 0) return d[x];
+	return d[x] = dp(x - 1) + dp(x - 2); //기록할 수 있도록
+}
+
+int main() {
+	cout << dp(50);
+	return 0;
+}
+
